86f8e05d608b767744f8b1a7f6a1cc7f9e967fd0
Your Name <jymohcooler@gmail.com>
2023-09-23 18:16:10 +0300

    added a read me

---
+++ server/app.py
@@ -0,0 +1,74 @@
+from flask import Flask,make_response,jsonify,request
+from flask_migrate import Migrate
+from flask_restful import Api,Resource
+
+from models import db,Restaurant,RestaurantPizza,PizzaModel
+
+app= Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pizza.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] =False
+app.json.compact = False
+
+migrate = Migrate(app,db)
+
+db.init_app(app)
+api = Api(app)
+
+class Home(Resource):
+    def get(self):
+        home_response = {"Home":"Restaurant API",}
+        response = make_response(jsonify(home_response),200,)
+        return response
+api.add_resource(Home,'/')
+
+class RestaurantResource(Resource):
+    def get(self):
+        restaurants = [{'id': restaurant.id, 'name': restaurant.name, 'address': restaurant.address} for restaurant in Restaurant.query.all()]
+        response = make_response(jsonify(restaurants), 200)
+        return response
+    
+api.add_resource(RestaurantResource,'/restaurants')    
+
+class RestaurantByIDResource(Resource):
+    def get(self,id):
+        restaurant = Restaurant.query.filter(Restaurant.id == id).first()
+        if not restaurant:
+            response_dict = {"error": "Restaurant not found"}
+            response = make_response(jsonify(response_dict),404)
+            return response
+        response = make_response(jsonify(restaurant.to_dict()),200)
+        return response
+    def delete(self,id):
+        restaurant = Restaurant.query.filter(Restaurant.id == id).first()
+        if not restaurant:
+            response_dict = {"error": "Restaurant not found"}
+            response = make_response(jsonify(response_dict),404)
+            return response
+        db.session.delete(restaurant)
+        db.session.commit()
+        response_dict = {"deleted":"deleted succesfully"}
+        response = make_response(jsonify(response_dict),200)
+        return response
+api.add_resource(RestaurantByIDResource,'/restaurants/<int:id>') 
+
+
+class RestaurantPizzaResource(Resource):
+    def post(self):
+        data = request.get_json()
+        restaurantpizza = RestaurantPizza(price= data['price'],pizza_id=data['pizza_id'],restaurant_id= data['restaurant_id'])
+        db.session.add(restaurantpizza)
+        db.session.commit()
+        response= make_response(jsonify(restaurantpizza.to_dict()),201)
+        return response
+
+api.add_resource(RestaurantPizzaResource,'/restaurant_pizza')
+
+class PizzaResource(Resource):
+    def get(self):
+        pizzas = [{'id':pizza.id,'name':pizza.name,'ingredients':pizza.ingredients} for pizza in PizzaModel.query.all()]
+        response = make_response(jsonify(pizzas),200)
+        return response
+api.add_resource(PizzaResource,'/pizza')
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
+from sqlalchemy import MetaData,UniqueConstraint,CheckConstraint
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.orm import validates
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -8,39 +9,48 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
-class Restaurant(db.Model,SerializerMixin):
+class Restaurant(db.Model, SerializerMixin):
     __tablename__ = 'restaurants'
 
     serialize_rules = ('-restaurant_pizzas.restaurant',)
 
-    id = db.Column(db.Integer,primary_key= True)
-    name= db.Column(db.String)
-    ingredients = db.Column(db.String)
-    created_at = db.Column(db.DateTime,server_default= db.func.now())
-    updated_at = db.Column(db.DateTime,onupdate= db.func.now())
-
-    restaurant_pizzas = db.relationship('RestaurantPizza',backref = 'restaurant',lazy='select')
-
-class RestaurantPizza(db.Model,SerializerMixin):
-    __tablename__ = 'restaurant_pizzas'
-    serialize_rules = ('-restaurant.restaurant_pizzas')
-    
-    id = db.Column(db.Integer,primary_key= True)
-    pizza_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
-    restaurant_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'))
-    price = db.Column(db.Integer)
-    created_at = db.Column(db.DateTime,server_default= db.func.now())
-    updated_at = db.Column(db.DateTime,onupdate= db.func.now())
-
-class Pizza(db.Model,SerializerMixin):
-    __tablename__ = 'pizzas'
-    
-    serialize_rules = ('-restaurant_pizzas.pizza')
-    id = db.Column(db.integer,primary_key= True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     address = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    restaurant_pizzas = db.relationship('RestaurantPizza',backref= 'pizza',lazy = 'select')
+    restaurant_pizzas = db.relationship('RestaurantPizza', backref='restaurant', lazy='select')
+
+    __table_args__ = (
+        UniqueConstraint('name',name="unique_name_constraint"),
+        CheckConstraint('LENGTH(name) <= 50',name="check_name_constraint")
+    )
+
+class RestaurantPizza(db.Model, SerializerMixin):
+    __tablename__ = 'restaurant_pizzas'
+    serialize_rules = ('-restaurant.restaurant_pizzas',)
+    
+    id = db.Column(db.Integer, primary_key=True)
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'))
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
+    price = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    @validates('price')
+    def validate_price(self, key, price):
+        if not (1 < price < 30):
+            raise ValueError("price must be between 1 and 30")
+        return price
 
 
+class PizzaModel(db.Model, SerializerMixin):
+    __tablename__ = 'pizzas'
+    
+    serialize_rules = ('-restaurant_pizzas.pizza',)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    ingredients = db.Column(db.String)
 
+    restaurant_pizzas = db.relationship('RestaurantPizza', backref='pizza', lazy='select')
